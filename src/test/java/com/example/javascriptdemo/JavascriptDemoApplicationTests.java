cd2017c518a61759e97cfa0a55651e54800e95dc
dat <datdta95@gmail.com>
2018-12-08 23:19:56 +0700

    test

---
+++ src/test/java/com/example/javascriptdemo/JavascriptDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.javascriptdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavascriptDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

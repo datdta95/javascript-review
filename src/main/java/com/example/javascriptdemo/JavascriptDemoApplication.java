e8eaa8e66977db1ccd388570f7d97e42301e6a66
dat <datdta95@gmail.com>
2018-12-08 23:20:05 +0700

    file main

---
+++ src/main/java/com/example/javascriptdemo/JavascriptDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.javascriptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptDemoApplication.class, args);
+	}
+}

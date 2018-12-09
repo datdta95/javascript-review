c4fb53ba1d11a8d67557fb44eabf9a430ca61884
dat <datdta95@gmail.com>
2018-12-09 13:22:04 +0700

    file js demo khi cong 2 so vao 1 chuoi

--- src/main/resources/static/js/jSDataTypes.js
+++ src/main/resources/static/js/jSDataTypes.js
@@ -1,6 +1,12 @@
 var x;
+
 function addNumberIntoString() {
     x = 3 + "some";
     // Tim phan tu HTML co id la demo sau do thay noi dung cua phan tu HTML do bang gia tri cua x
     document.getElementById("demo").innerHTML = x;
+}
+
+function addNumberIntoStringWithOperator() {
+    x = 1 + 2 + "some";
+    document.getElementById("demo2").innerHTML = x;
 }

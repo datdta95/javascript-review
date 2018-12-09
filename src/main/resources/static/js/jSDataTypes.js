72ef4ea2b2db57f114bfb6bf4d76d1515bb65f38
dat <datdta95@gmail.com>
2018-12-09 13:16:20 +0700

    file js demo khi cong 1 so vao 1 chuoi

---
+++ src/main/resources/static/js/jSDataTypes.js
@@ -0,0 +1,6 @@
+var x;
+function addNumberIntoString() {
+    x = 3 + "some";
+    // Tim phan tu HTML co id la demo sau do thay noi dung cua phan tu HTML do bang gia tri cua x
+    document.getElementById("demo").innerHTML = x;
+}

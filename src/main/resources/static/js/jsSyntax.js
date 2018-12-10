5a8a5f531a6ee7af86e5541618e1fc6f28fe3355
SON NGUYEN <myantj3m@gmail.com>
2018-12-10 11:15:23 +0700

    JS Syntax done

---
+++ src/main/resources/static/js/jsSyntax.js
@@ -0,0 +1,8 @@
+document.getElementById('demo1').innerHTML = 10;
+document.getElementById('demo2').innerHTML = 5.5;
+document.getElementById('demo3').innerHTML = "Nháy kép";
+document.getElementById('demo4').innerHTML = 'Nháy đơn nè';
+document.getElementById('demo').innerHTML = "Cú pháp của JavaScript là tập hợp các quy tắc, cách các chương trình JavaScript được xây dựng:\n" +
+    "var x, y;          //  Cách khai báo biến\n" +
+    "x = 5; y = 6;      //  Cách gán giá trị\n" +
+    "z = x + y;         //  Cách tính giá trị";
